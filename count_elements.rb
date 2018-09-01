67c07f4676690fe14e3d2d447c7b7d1f7afa86ff
Trevor Jameson <github email address>
2018-09-01 12:31:59 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  counted_hash = {}
+  array.each do |elem|
+    if counted_hash[elem]
+      counted_hash[elem] += 1
+    else
+      counted_hash[elem] = 1
+    end
+  end
+  counted_hash
 end
- 
